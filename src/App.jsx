130aa5f579c8cbe5da1e026e7e5ef658a3775687
CarlosCallejaSaez <carloscallejasez@gmail.com>
2024-02-12 12:16:48 +0100

    restyled done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Select,Box, Heading, Input, Text,Center } from '@chakra-ui/react';
+import { ChakraProvider, CSSReset, Box, Select, Heading, Input, Text, Center } from '@chakra-ui/react';
 import Figure from './components/Figure';
+import "./App.css"
+import ReactPlayer from 'react-player';
 
 function App() {
   const today = new Date().toISOString().slice(0, 10);
@@ -8,60 +10,90 @@ function App() {
   const [data, setData] = useState([]);
   const [dataMars, setDataMars] = useState([]);
   const [api, setApi] = useState("apod");
+  const isDesktop = window.innerWidth > 500;
 
   const NASA_API_KEY = "ss9M5D2dUzV3Ppryxhsm56x71CcG7OTQfrrwC9U4";
-
   const baseUrlApod = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}&date=${date}&hd=true`;
-  const baseUrlMars= `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${NASA_API_KEY}`
+  const baseUrlMars = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${NASA_API_KEY}`;
 
   useEffect(() => {
-    if (api === "apod"){
-    const APODFetching = async () => {
-      const response = await fetch(baseUrlApod);
-      const data = await response.json();
-      setData(data);
-    };
-    APODFetching();}
-    else if(api === "mars"){
-      const marsFetching =async () =>{
+    if (api === "apod") {
+      const APODFetching = async () => {
+        const response = await fetch(baseUrlApod);
+        const data = await response.json();
+        setData(data);
+      };
+      APODFetching();
+    } else if (api === "mars") {
+      const marsFetching = async () => {
         const response = await fetch(baseUrlMars);
-      const data = await response.json();
-      setDataMars(data.photos[0]);
-      console.log("mars",data)
-      }
-      marsFetching()
-  }
-  }, [api,date]);
+        const data = await response.json();
+        setDataMars(data.photos[0]);
+      };
+      marsFetching();
+    }
+  }, [api, date]);
 
   const handleInput = (ev) => {
     setDate(ev.target.value);
   };
 
+  
   return (
-    <Box p={4}>
-      <Select placeholder='Selecciona API'  onChange={(ev) => setApi(ev.target.value)}>
-  <option value='apod'>Apod</option>
-  <option value='mars'>Mars</option>
-</Select>
-      <Heading as="h1" size="xl" mb={4} textAlign="center">
-        Imagen astronómica del día
-      </Heading>
-      <Text mb={2} textAlign="center">Esta imagen corresponde a la fecha {date}</Text>
-      <Center>
-      <Input type="date" max={today} onChange={handleInput} mb={2} size="sm" textAlign="center" width="200px"/>
-      </Center>
-
-      {api==="apod" && <Figure data={data}/> }
-      {api==="mars" && <Figure dataMars={dataMars} />}
-      
-
+    <ChakraProvider>
+      <CSSReset />
+      <Box minHeight="100vh" bgImage="url('./stars.gif')" bgPosition="center" bgRepeat="no-repeat" bgSize="cover" position="relative">
      
+        <Box
+          position="absolute"
+          top="30%"
+          left="90%"
+          transform="translate(-50%, -50%)"
+          animation="float 5s infinite"
+          zIndex="1"
+        >
+          <img src="./nasa.png" alt="NASA Logo" width="100" height="100" />
+        </Box>
 
+        <Box p={4}   rounded="md">
+          <Center>
+            <Select placeholder='Select API' onChange={(ev) => setApi(ev.target.value)} color={'blackeAlpha.900'} backgroundColor={'whiteAlpha.900'} width={300}>
+              <option value='apod'  >Apod</option>
+              <option value='mars'>Mars</option>
+            </Select>
+          </Center>
+          <Heading as="h1" size="xl" mb={4} textAlign="center" color={'whiteAlpha.800'}>
+            Astronomical Image of the Day
+          </Heading>
+          <Text mb={2} textAlign="center" color={'whiteAlpha.800'} >This image corresponds to the date {date}</Text>
+          <Center>
+            <Text mb={2} textAlign="center" color={'whiteAlpha.800'} >Select other date <br/> </Text>
+            <br/>  
+          </Center>
+          <Center>
+            <Input type="date" max={today} onChange={handleInput}  color="whiteAlpha.900" mb={2} size="md" textAlign="center" width="200px"/>
+          </Center>
 
-
-
-      
-    </Box>
+          {api==="apod" && <Figure data={data}/> }
+          {api==="mars" && <Figure dataMars={dataMars} />}
+          <Box
+            position={isDesktop ? "fixed" : "relative"}
+            bottom="20px"
+            left={isDesktop ? "20px" : "auto"}
+            right={isDesktop ? "auto" : "0"}
+            width={isDesktop ? "200px" : "100px"}
+            zIndex="999"
+          >
+            <ReactPlayer
+              url="./universe.mp3"
+              width="100%"
+              height="100px"
+              controls
+            />
+          </Box>
+        </Box>
+      </Box>
+    </ChakraProvider>
   );
 }
 
--- src/components/Figure.jsx
+++ src/components/Figure.jsx
@@ -1,29 +1,65 @@
 import React from 'react';
-import { Box, Heading, Image, Text, Center } from '@chakra-ui/react';
-import App from '../App.css';
+import { Box, Heading, Image, Text, Center, AspectRatio, VStack, useBreakpointValue } from '@chakra-ui/react';
 
-const Figure = ({data,dataMars}) => {
-    if (data){
-  return (
-    <Box>
-      <Heading as="h2" size="lg" mb={4} textAlign="center">{data.title}</Heading>
+const Figure = ({ data, dataMars }) => {
+  
+  const headingSize = useBreakpointValue({ base: 'md', md: 'lg', lg: 'xl' });
+  const textSize = useBreakpointValue({ base: 'md', md: 'lg' });
 
-      {data.media_type === 'image' && <Image src={data.url} alt={data.title} mb={4} mx="auto"/>}  
-      {data.media_type === 'video' && <Center><iframe src={data.url} title={data.title} /></Center>}
+  if (data) {
+    return (
+      <VStack spacing={4} align="center" justify="center" minHeight="100vh" paddingX={4} >
+        <Heading as="h2" size={headingSize} textAlign="center" noOfLines={2} maxWidth="90%" color="whiteAlpha.900" style={{
+        background: 'linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2))',
+        backdropFilter: 'blur(10px)',
+        borderRadius: '10px', 
+        boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)', 
+      }}>
+          {data.title}
+        </Heading>
 
-      <Text fontSize="sm" fontWeight="bold" mb={2} textAlign="center" >© {data.copyright}</Text>
-      <Text>{data.explanation}</Text>
-      
-    </Box>
-  );}
+        {data.media_type === 'image' ? (
+          <Image src={data.url} alt={data.title} maxH="70vh" objectFit="contain" rounded="md" boxShadow="xl"/>
+        ) : (
+          <AspectRatio ratio={16 / 9} maxWidth="95%" width="100%" maxHeight="70vh">
+            <iframe src={data.url} title={data.title} allowFullScreen />
+          </AspectRatio>
+        )}
 
-  if(dataMars){
-    return (<div className='mars'>
-        <img src={dataMars.img_src}/>  
-        </div>
+        <Text fontSize={textSize} fontWeight="bold"  color="whiteAlpha.900" style={{
+        background: 'linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2))',
+        backdropFilter: 'blur(10px)',
+        borderRadius: '10px', 
+        boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)', 
+      }}>
+          © {data.copyright}
+        </Text>
+      <Text textAlign="center" size={"lg"}  px={2} maxWidth="70%" color="whiteAlpha.900" style={{
+        background: 'linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2))',
+        backdropFilter: 'blur(1000px)',
+        borderRadius: '10px', 
+        boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)', 
+        
+      }}>
+          {data.explanation}
+        
 
-    )
+          
+          
+        </Text>
+      </VStack>
+    );
   }
+
+  if (dataMars) {
+    return (
+      <Center minHeight="100vh" flexDirection="column" paddingX={4}>
+        <Image src={dataMars.img_src} alt="Mars Rover Image" maxH="70vh" objectFit="contain" rounded="md" boxShadow="xl" marginBottom={4}/>
+      </Center>
+    );
+  }
+
+  return null;
 };
 
 export default Figure;
